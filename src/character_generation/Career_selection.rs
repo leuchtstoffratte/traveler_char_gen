146031a9c9f58b102e7a98d563e4e0fdf801ae49
leuchtstoffratte <kitersquirel@web.de>
2023-05-22 00:01:27 +0200

    Added command line input for first primitiv steering of career path

---
+++ src/character_generation/Career_selection.rs
@@ -0,0 +1,15 @@
+use crate::CharacterSheet;
+use super::Career;
+
+
+pub fn select_new_carreer(sheet: &mut CharacterSheet, careers : &mut Vec<Career> ) -> Option<Career>{
+
+    //TODO:
+    //print list of available carrers
+    //allow selection
+    //try to succeed
+    //branch out if that does not work
+    //remove selected carrer from available carrers and return selected carrer
+    
+    None
+}
--- src/character_generation/mod.rs
+++ src/character_generation/mod.rs
@@ -1,6 +1,8 @@
-use std::fmt;
+mod Career_selection;
 
-use crate::mechanix::AttributeThrow;
+use std::{fmt};
+
+use crate::{mechanix::AttributeThrow, character_sheet::CharacterSheet, import_game_info::import_careers};
 
 
 pub struct Career {
@@ -31,8 +33,9 @@ pub struct Enhancement{
     enhancement_type : EnhancementType,
     enhanced_property : String,
     bonus : u8
-
 }
+
+
 
 enum EnhancementType{
     Attribute,
@@ -40,5 +43,80 @@ enum EnhancementType{
     BoundedSkill,
     Specialization,
     BoundedSpecialization
+}
+
+
+#[derive(Debug, Default)]
+enum NextCarreerStep{
+    ContinueCurrent,
+    BranchToNew,
+    GetDrafted,
+    FinalizeChar,
+    #[default]
+    Noop,
+}
+
+// impl Default for NextCarreerStep {
+    
+// }
+
+
+
+pub fn generate_char() -> CharacterSheet{
+
+    let mut my_new_character : CharacterSheet = CharacterSheet::roll_up_new_basic_char();
+
+    let mut careers : Vec<Career> = import_careers();
+
+    let mut current_career : Option<Career> = None;
+
+    loop {
+        match read_next_step_from_cli() {
+                NextCarreerStep::Noop => println!("printing help"),
+                NextCarreerStep::BranchToNew => current_career = Career_selection::select_new_carreer(&mut my_new_character, &mut careers),
+                NextCarreerStep::GetDrafted => println!("joining up"),
+                NextCarreerStep::ContinueCurrent => println!("Continuing"),
+                NextCarreerStep::FinalizeChar => {println!("finalizing"); break},
+
+        }
+
+        println!("in da loop \n char looks like {}", &my_new_character);
+
+
+    }
+
+    my_new_character
+}
+
+
+///Parse user enties
+fn read_next_step_from_cli() -> NextCarreerStep{
+
+    let mut line = String::new();
+
+    let mut next_step : Option<NextCarreerStep> = None;
+
+    while next_step.is_none() {
+
+        line = String::new(); //clear buffer
+
+        std::io::stdin().read_line(&mut line).expect("Should have been a valid next step in your carreer.");
+
+        next_step =  match line.trim() {
+            "C" => Some(NextCarreerStep::ContinueCurrent),
+            "c" => Some(NextCarreerStep::ContinueCurrent),
+            "N" => Some(NextCarreerStep::BranchToNew),
+            "n" => Some(NextCarreerStep::BranchToNew),
+            "F" => Some(NextCarreerStep::FinalizeChar),
+            "f" => Some(NextCarreerStep::FinalizeChar),
+            "D" => Some(NextCarreerStep::GetDrafted),
+            "d" => Some(NextCarreerStep::GetDrafted),
+            _ => Some(NextCarreerStep::Noop)
+        }
+
+    }
+
+    next_step.unwrap_or_default()
+
 }
 
